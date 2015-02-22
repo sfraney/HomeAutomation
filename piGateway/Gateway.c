b8b02435fa67b9da6a2d7adcf94a55bbf836d222
Sean Franey <sean.b.franey@gmail.com>
2015-02-22 00:39:59 -0800

    Modify Gateway - mostly formatting

--- piGateway/Gateway.c
+++ piGateway/Gateway.c
@@ -1,28 +1,27 @@
 /*
-RFM69 Gateway RFM69 pushing the data to the mosquitto server
-by Alexandre Bouillot
+  RFM69 Gateway RFM69 pushing the data to the mosquitto server
+  by Alexandre Bouillot
 
-License:  CC-BY-SA, https://creativecommons.org/licenses/by-sa/2.0/
-Date:  10-23-2014
-File: Gateway.ino
-This sketch receives RFM wireless data and forwards it to Mosquitto relay
+  License:  CC-BY-SA, https://creativecommons.org/licenses/by-sa/2.0/
+  Date:  10-23-2014
+  File: Gateway.ino
+  This sketch receives RFM wireless data and forwards it to Mosquitto relay
 
-sensorNode.sensorID,
+  sensorNode.sensorID,
 
-Modifications Needed:
-1)  Update encryption string "ENCRYPTKEY"
+  Modifications Needed:
+  1)  Update encryption string "ENCRYPTKEY"
 */
 
 /*
-RFM69 Pinout:
-MOSI = 11
-MISO = 12
-SCK = 13
-SS = 8
+  RFM69 Pinout:
+  MOSI = 11
+  MISO = 12
+  SCK = 13
+  SS = 8
 */
 
 //general --------------------------------
-#define SERIAL_BAUD   115200
 #if 1
 #define DEBUG1(expression)  fprintf(stderr, expression)
 #define DEBUG2(expression, arg)  fprintf(stderr, expression, arg)
@@ -48,14 +47,13 @@ SS = 8
 #include <errno.h>
 
 #define NODEID        1    //unique for each node on same network
-#define NETWORKID     101  //the same on all nodes that talk to each other
+#define NETWORKID     10   //the same on all nodes that talk to each other
 #define FREQUENCY   RF69_433MHZ
 //#define FREQUENCY   RF69_868MHZ
-//#define FREQUENCY     RF69_915MHZ
-#define ENCRYPTKEY    "xxxxxxxxxxxxxxxx" //exactly the same 16 characters/bytes on all nodes!
-#define IS_RFM69HW    //uncomment only for RFM69HW! Leave out if you have RFM69W!
+//#define FREQUENCY   RF69_915MHZ
+#define ENCRYPTKEY    "GRACEVICTAFRANEY" //exactly the same 16 characters/bytes on all nodes!
+#define IS_RFM69HW       //uncomment only for RFM69HW! Leave out if you have RFM69W!
 #define ACK_TIME      30 // max # of ms to wait for an ack
-#define RFM69_SS  8
 
 RFM69 *rfm69;
 bool promiscuousMode = false; //set to 'true' to sniff all packets on the same network
@@ -65,7 +63,7 @@ byte ackCount=0;
 #include <mosquitto.h>
 
 /* How many seconds the broker should wait between sending out
-* keep-alive messages. */
+ * keep-alive messages. */
 #define KEEPALIVE_SECONDS 60
 /* Hostname and port for the MQTT broker. */
 #define BROKER_HOSTNAME "localhost"
@@ -76,25 +74,23 @@ byte ackCount=0;
 
 int sendMQTT = 0;
 
-typedef struct {		
-	short           nodeID; 
-	short			sensorID;
-	unsigned long   var1_usl; 
-	float           var2_float; 
-	float			var3_float;	
-} 
-Payload;
+typedef struct {
+  short nodeID;			//node ID (1xx, 2xx, 3xx);  1xx = basement, 2xx = main floor, 3xx = outside
+  short sensorID;		//sensor ID (2, 3, 4, 5)
+  unsigned long uptime_usl;	//uptime in ms
+  float sens_dat_flt;		//sensor data?
+  float batt_con_flt;		//battery condition?
+} Payload;
 Payload theData;
 
 typedef struct {
-	short           nodeID;
-	short			sensorID;		
-	unsigned long   var1_usl;
-	float           var2_float;
-	float			var3_float;		//
-	int             var4_int;
-}
-SensorNode;
+  short nodeID;
+  short sensorID;
+  unsigned long uptime_usl;
+  float sens_dat_flt;
+  float batt_con_flt;
+  int var4_int;
+} SensorNode;
 SensorNode sensorNode;
 
 static void die(const char *msg);
@@ -107,206 +103,203 @@ static void MQTTSendULong(struct mosquitto* _client, int node, int sensor, int v
 static void MQTTSendFloat(struct mosquitto* _client, int node, int sensor, int var, float val);
 
 static void uso(void) {
-	fprintf(stderr, "Use:\n Simply use it without args :D\n");
-	exit(1);
+  fprintf(stderr, "Use:\n Simply use it without args :D\n");
+  exit(1);
 }
 
 int main(int argc, char* argv[]) {
-	if (argc != 1) uso();
-	int i;
-	long packetCount = 0;
+  if (argc != 1) uso();
 
-	struct mosquitto *m = mosquitto_new(MQTT_CLIENT_ID, true, null);
-	if (m == NULL) { die("init() failure\n"); }
+  struct mosquitto *mqtt = mosquitto_new(MQTT_CLIENT_ID, true, null);
+  if (mqtt == NULL) { die("mosquitto init() failure\n"); }
 
-	if (!set_callbacks(m)) { die("set_callbacks() failure\n"); }
-	if (!connect(m)) { die("connect() failure\n"); }
+  if (!set_callbacks(mqtt)) { die("set_callbacks() failure\n"); }
+  if (!connect(mqtt)) { die("connect() failure\n"); }
 
-	//RFM69 ---------------------------
-	rfm69 = new RFM69();
-	rfm69->initialize(FREQUENCY,NODEID,NETWORKID);
-	#ifdef IS_RFM69HW
-	rfm69->setHighPower(); //uncomment only for RFM69HW!
-	#endif
-	rfm69->encrypt(ENCRYPTKEY);
-	rfm69->promiscuous(promiscuousMode);
-	LOG("\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
+  //RFM69 ---------------------------
+  rfm69 = new RFM69();
+  rfm69->initialize(FREQUENCY,NODEID,NETWORKID);
+#ifdef IS_RFM69HW
+  rfm69->setHighPower(); //uncomment only for RFM69HW!
+#endif
+  rfm69->encrypt(ENCRYPTKEY);
+  rfm69->promiscuous(promiscuousMode);
+  LOG("\nListening at %d Mhz...", FREQUENCY==RF69_433MHZ ? 433 : FREQUENCY==RF69_868MHZ ? 868 : 915);
 
-	LOG("setup complete\n");
+  LOG("setup complete\n");
 
-	return run_loop(m);
+  return run_loop(mqtt);
 }  // end of setup
 
 /* Loop until it is explicitly halted or the network is lost, then clean up. */
-static int run_loop(struct mosquitto *m) {
-	int res;
-	for (;;) {
-		res = mosquitto_loop(m, 1000, 1);
+static int run_loop(struct mosquitto *mqtt) {
+  int res;
+  while(1) {
+    res = mosquitto_loop(mqtt, 1000, 1);
 
-		if (rfm69->receiveDone()) {
-			LOG("[%d] ",rfm69->SENDERID);
-			if (promiscuousMode) {
-				LOG(" to [%d] ", rfm69->TARGETID);
-			}
+    if (rfm69->receiveDone()) {
+      LOG("[%d] ",rfm69->SENDERID);
+      if (promiscuousMode) {
+	LOG(" to [%d] ", rfm69->TARGETID);
+      }
 
-			for(int i = 0; i < rfm69->DATALEN; i++) {
-				LOG("%x.", rfm69->DATA[i]); }
-			LOG("\n");
+      for(int i = 0; i < rfm69->DATALEN; i++) {
+	LOG("%x.", rfm69->DATA[i]);
+      }
+      LOG("\n");
 
-			if (rfm69->DATALEN != sizeof(Payload)) {
-				LOG("Invalid payload received, not matching Payload struct! %d - %d\r\n", rfm69->DATALEN, sizeof(Payload));
-			}
-			else {
-				theData = *(Payload*)rfm69->DATA; //assume radio.DATA actually contains our struct and not something else
+      if (rfm69->DATALEN != sizeof(Payload)) {
+	LOG("Invalid payload received, not matching Payload struct! %d - %d\r\n", rfm69->DATALEN, sizeof(Payload));
+      } else {
+	theData = *(Payload*)rfm69->DATA; //assume radio.DATA actually contains our struct and not something else
 
-				//save it for i2c:
-				sensorNode.nodeID = theData.nodeID;
-				sensorNode.sensorID = theData.sensorID;
-				sensorNode.var1_usl = theData.var1_usl;
-				sensorNode.var2_float = theData.var2_float;
-				sensorNode.var3_float = theData.var3_float;
-				sensorNode.var4_int = rfm69->RSSI;
+	sensorNode.nodeID = theData.nodeID;
+	sensorNode.sensorID = theData.sensorID;
+	sensorNode.uptime_usl = theData.uptime_usl;
+	sensorNode.sens_dat_flt = theData.sens_dat_flt;
+	sensorNode.batt_con_flt = theData.batt_con_flt;
+	sensorNode.var4_int = rfm69->RSSI;
 
-				LOG("Received Node ID = %d Device ID = %d Time = %d  RSSI = %d var2 = %f var3 = %f\n",
-					sensorNode.nodeID,
-					sensorNode.sensorID,
-					sensorNode.var1_usl,
-					sensorNode.var4_int,
-					sensorNode.var2_float,
-					sensorNode.var3_float
-				);
-				sendMQTT = 1;
-			}
+	LOG("Received Node ID = %d Device ID = %d Time = %d  RSSI = %d var2 = %f var3 = %f\n",
+	    sensorNode.nodeID,
+	    sensorNode.sensorID,
+	    sensorNode.uptime_usl,
+	    sensorNode.var4_int,
+	    sensorNode.sens_dat_flt,
+	    sensorNode.batt_con_flt
+	    );
+	sendMQTT = 1;
+      }
 
-		if (rfm69->ACK_REQUESTED) {
-			byte theNodeID = rfm69->SENDERID;
-			rfm69->sendACK();
+      if (rfm69->ACK_REQUESTED) {
+	byte theNodeID = rfm69->SENDERID;
+	rfm69->sendACK();
 
-			// When a node requests an ACK, respond to the ACK
-			// and also send a packet requesting an ACK (every 3rd one only)
-			// This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
-			if (ackCount++%3==0) {
-				//Serial.print(" Pinging node ");
-				//Serial.print(theNodeID);
-				//Serial.print(" - ACK...");
-				//delay(3); //need this when sending right after reception .. ?
-				//if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0))  // 0 = only 1 attempt, no retries
-				//  Serial.print("ok!");
-				//else Serial.print("nothing");
-			}
-		}//end if radio.ACK_REQESTED
-	} //end if radio.receive
-
-	if (sendMQTT == 1) {
-		//send var1_usl
-		MQTTSendULong(m, sensorNode.nodeID, sensorNode.sensorID, 1, sensorNode.var1_usl);
-
-		//send var2_float
-		MQTTSendFloat(m, sensorNode.nodeID, sensorNode.sensorID, 2, sensorNode.var2_float);
-
-		//send var3_float
-		MQTTSendFloat(m, sensorNode.nodeID, sensorNode.sensorID, 3, sensorNode.var3_float);
-
-		//send var4_int, RSSI
-		MQTTSendInt(m, sensorNode.nodeID, sensorNode.sensorID, 4, sensorNode.var4_int);
-
-		sendMQTT = 0;
-	}//end if sendMQTT
+	// When a node requests an ACK, respond to the ACK
+	// and also send a packet requesting an ACK (every 3rd one only)
+	// This way both TX/RX NODE functions are tested on 1 end at the GATEWAY
+	if (ackCount++%3==0) {
+	  //Serial.print(" Pinging node ");
+	  //Serial.print(theNodeID);
+	  //Serial.print(" - ACK...");
+	  //delay(3); //need this when sending right after reception .. ?
+	  //if (radio.sendWithRetry(theNodeID, "ACK TEST", 8, 0))  // 0 = only 1 attempt, no retries
+	  //  Serial.print("ok!");
+	  //else Serial.print("nothing");
 	}
+      }//end if radio.ACK_REQESTED
+    } //end if radio.receive
 
-	mosquitto_destroy(m);
-	(void)mosquitto_lib_cleanup();
+    if (sendMQTT == 1) {
+      //send uptime_usl
+      MQTTSendULong(mqtt, sensorNode.nodeID, sensorNode.sensorID, 1, sensorNode.uptime_usl);
 
-	if (res == MOSQ_ERR_SUCCESS) {
-		return 0;
-	} else {
-		return 1;
-	}
+      //send sens_dat_flt
+      MQTTSendFloat(mqtt, sensorNode.nodeID, sensorNode.sensorID, 2, sensorNode.sens_dat_flt);
+
+      //send batt_con_flt
+      MQTTSendFloat(mqtt, sensorNode.nodeID, sensorNode.sensorID, 3, sensorNode.batt_con_flt);
+
+      //send var4_int, RSSI
+      MQTTSendInt(mqtt, sensorNode.nodeID, sensorNode.sensorID, 4, sensorNode.var4_int);
+
+      sendMQTT = 0;
+    }//end if sendMQTT
+  }
+
+  mosquitto_destroy(mqtt);
+  (void)mosquitto_lib_cleanup();
+
+  if (res == MOSQ_ERR_SUCCESS) {
+    return 0;
+  } else {
+    return 1;
+  }
 }
 
 
 static void MQTTSendInt(struct mosquitto * _client, int node, int sensor, int var, int val) {
-	char buff_topic[6];
-	char buff_message[7];
+  char buff_topic[6];
+  char buff_message[7];
 
-	sprintf(buff_topic, "%02d%01d%01d", node, sensor, var);
-	sprintf(buff_message, "%04d%", val);
-	mosquitto_publish(_client, 0, &buff_topic[0], strlen(buff_message), buff_message, 0, false);
+  sprintf(buff_topic, "%02d%01d%01d", node, sensor, var);
+  sprintf(buff_message, "%04d%", val);
+  mosquitto_publish(_client, 0, &buff_topic[0], strlen(buff_message), buff_message, 0, false);
 }
 
 static void MQTTSendULong(struct mosquitto* _client, int node, int sensor, int var, unsigned long val) {
-	char buff_topic[6];
-	char buff_message[12];
+  char buff_topic[6];
+  char buff_message[12];
 
-	sprintf(buff_topic, "%02d%01d%01d", node, sensor, var);
-	sprintf(buff_message, "%u", val);
-	mosquitto_publish(_client, 0, &buff_topic[0], strlen(buff_message), buff_message, 0, false);
+  sprintf(buff_topic, "%02d%01d%01d", node, sensor, var);
+  sprintf(buff_message, "%u", val);
+  mosquitto_publish(_client, 0, &buff_topic[0], strlen(buff_message), buff_message, 0, false);
 }
 
 static void MQTTSendFloat(struct mosquitto* _client, int node, int sensor, int var, float val) {
-	char buff_topic[6];
-	char buff_message[12];
+  char buff_topic[6];
+  char buff_message[12];
 
-	sprintf(buff_topic, "%02d%01d%01d", node, sensor, var);
-	snprintf(buff_message, 12, "%f", val);
+  sprintf(buff_topic, "%02d%01d%01d", node, sensor, var);
+  snprintf(buff_message, 12, "%f", val);
 
-	mosquitto_publish(_client, 0, buff_topic, strlen(buff_message), buff_message, 0, false);
+  mosquitto_publish(_client, 0, buff_topic, strlen(buff_message), buff_message, 0, false);
 }
 
 // Handing of Mosquitto messages
 void callback(char* topic, byte* payload, unsigned int length) {
-	// handle message arrived
-	LOG("Mosquitto Callback\n");
+  // handle message arrived
+  LOG("Mosquitto Callback\n");
 }
 
 /* Fail with an error message. */
 static void die(const char *msg) {
-	fprintf(stderr, "%s", msg);
-	exit(1);
+  fprintf(stderr, "%s", msg);
+  exit(1);
 }
 
 /* Connect to the network. */
 static bool connect(struct mosquitto *m) {
-	int res = mosquitto_connect(m, BROKER_HOSTNAME, BROKER_PORT, KEEPALIVE_SECONDS);
-	return res == MOSQ_ERR_SUCCESS;
+  int res = mosquitto_connect(m, BROKER_HOSTNAME, BROKER_PORT, KEEPALIVE_SECONDS);
+  return res == MOSQ_ERR_SUCCESS;
 }
 
 /* Callback for successful connection: add subscriptions. */
 static void on_connect(struct mosquitto *m, void *udata, int res) {
-	if (res == 0) {             /* success */
-		LOG("Connect succeed\n");
-	} else {
-		die("connection refused\n");
-	}
+  if (res == 0) {             /* success */
+    LOG("Connect succeed\n");
+  } else {
+    die("connection refused\n");
+  }
 }
 
 /* Handle a message that just arrived via one of the subscriptions. */
 static void on_message(struct mosquitto *m, void *udata,
-const struct mosquitto_message *msg) {
-	if (msg == NULL) { return; }
-	LOG("-- got message @ %s: (%d, QoS %d, %s) '%s'\n",
-		msg->topic, msg->payloadlen, msg->qos, msg->retain ? "R" : "!r",
-		msg->payload);
+		       const struct mosquitto_message *msg) {
+  if (msg == NULL) { return; }
+  LOG("-- got message @ %s: (%d, QoS %d, %s) '%s'\n",
+      msg->topic, msg->payloadlen, msg->qos, msg->retain ? "R" : "!r",
+      msg->payload);
 
 }
 
 /* A message was successfully published. */
 static void on_publish(struct mosquitto *m, void *udata, int m_id) {
-	LOG("-- published successfully\n");
+  LOG("-- published successfully\n");
 }
 
 /* Successful subscription hook. */
 static void on_subscribe(struct mosquitto *m, void *udata, int mid,
-		int qos_count, const int *granted_qos) {
-	LOG("-- subscribed successfully\n");
+			 int qos_count, const int *granted_qos) {
+  LOG("-- subscribed successfully\n");
 }
 
 /* Register the callbacks that the mosquitto connection will use. */
 static bool set_callbacks(struct mosquitto *m) {
-	mosquitto_connect_callback_set(m, on_connect);
-	mosquitto_publish_callback_set(m, on_publish);
-	mosquitto_subscribe_callback_set(m, on_subscribe);
-	mosquitto_message_callback_set(m, on_message);
-return true;
+  mosquitto_connect_callback_set(m, on_connect);
+  mosquitto_publish_callback_set(m, on_publish);
+  mosquitto_subscribe_callback_set(m, on_subscribe);
+  mosquitto_message_callback_set(m, on_message);
+  return true;
 }
 
